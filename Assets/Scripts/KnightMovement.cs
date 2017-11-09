6595d6324a08fa2e15b36808bba794b7cca985e9
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2017-11-08 22:10:02 -0700

    Initial commit with working walk/run animation

---
+++ Assets/Scripts/KnightMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightMovement : MonoBehaviour {
+
+    Animator anim;
+    int jumpHash = Animator.StringToHash("Jump");
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Vertical");
+        anim.SetFloat("Speed", move);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(jumpHash);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("Sprint", true);
+        } else
+        {
+            anim.SetBool("Sprint", false);
+        }
+
+
+	}
+}
